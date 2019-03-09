86b991b6be0ccff6ddaaff55bc83abdc5784c845
Luke-Tran <luketran08@gmail.com>
2019-03-09 09:39:56 -0500

    Made it so pieces 'stack'. Changed piece color to orange.

--- src/main/java/LukeTetrisPrototype/TetrisBoard.java
+++ src/main/java/LukeTetrisPrototype/TetrisBoard.java
@@ -31,21 +31,33 @@ public class TetrisBoard {
             }
         }
 
-        setOccupied(5, 5);
+        for (int i = 0; i < columnHeightArray.length; i++) {
+            columnHeightArray[i] = boardHeight;
+        }
+        
+        spawnPiece();
+    }
+
+    public void spawnPiece() {
+        setOccupied(0, 5);
     }
 
     public void setOccupied(int y, int x) {
-        if (x <= 0 || x >= rectArray[0].length || y >= rectArray.length) {
+        if (y >= columnHeightArray[occupiedTileX]) {
+            // Freeze the shape in place then spawn a new one
+            columnHeightArray[occupiedTileX] -= 1;
+            occupiedTileY = 0;
+            occupiedTileX = 5;
+            spawnPiece();
+            return;
+        }
+        if (x < 0 || x >= rectArray[0].length) {
             return;
         }
 
-        for (int h = 0; h < rectArray.length; h++) {
-            for (int w = 0; w < rectArray[h].length; w++) {
-                rectArray[h][w].setFill(Color.WHITE);
-            }
-        }
+        rectArray[occupiedTileY][occupiedTileX].setFill(Color.WHITE);
         occupiedTileY = y;
         occupiedTileX = x;
-        rectArray[y][x].setFill(Color.BLACK);
+        rectArray[y][x].setFill(Color.ORANGE);
     }
 }
