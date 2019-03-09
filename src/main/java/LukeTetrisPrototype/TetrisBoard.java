178fd56a97afeed67a573890b0594efceb3b566a
Luke-Tran <luketran08@gmail.com>
2019-03-09 11:56:20 -0500

    Added line clearing functionality. Refactored by renaming variables.

--- src/main/java/LukeTetrisPrototype/TetrisBoard.java
+++ src/main/java/LukeTetrisPrototype/TetrisBoard.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Paint;
 import javafx.scene.paint.Color;
 
 public class TetrisBoard {
@@ -13,28 +14,28 @@ public class TetrisBoard {
     int boardWidth = 10;
     int boardHeight = 25;
     int[] columnHeightArray = new int[boardWidth];
-    int[][] boardArray = new int[boardHeight][boardWidth];
-    Rectangle[][] rectArray = new Rectangle[boardHeight][boardWidth];
+    //int[][] boardArray = new int[boardHeight][boardWidth];
+    Rectangle[][] boardArray = new Rectangle[boardHeight][boardWidth];
     double tileSize = 15;
     int occupiedTileY;
     int occupiedTileX;
 
     public TetrisBoard() {
-        for (int h = 0; h < boardArray.length; h++) {
-            for (int w = 0; w < boardArray[h].length; w++) {
-                Rectangle r = new Rectangle(tileSize, tileSize, Color.WHITE);
-                r.setStroke(Color.BLACK);
-                r.setX(w*tileSize);
-                r.setY(h*tileSize);
-                rectArray[h][w] = r;
-                pane.getChildren().add(r);
+        for (int r = 0; r < boardArray.length; r++) {
+            for (int c = 0; c < boardArray[r].length; c++) {
+                Rectangle tile = new Rectangle(tileSize, tileSize, Color.WHITE);
+                tile.setStroke(Color.BLACK);
+                tile.setX(c*tileSize);
+                tile.setY(r*tileSize);
+                boardArray[r][c] = tile;
+                pane.getChildren().add(tile);
             }
         }
 
         for (int i = 0; i < columnHeightArray.length; i++) {
             columnHeightArray[i] = boardHeight;
         }
-        
+
         spawnPiece();
     }
 
@@ -48,16 +49,50 @@ public class TetrisBoard {
             columnHeightArray[occupiedTileX] -= 1;
             occupiedTileY = 0;
             occupiedTileX = 5;
+            checkForFilledRows();
             spawnPiece();
             return;
         }
-        if (x < 0 || x >= rectArray[0].length) {
+        if (x < 0 || x >= boardArray[0].length) {
             return;
         }
 
-        rectArray[occupiedTileY][occupiedTileX].setFill(Color.WHITE);
+        boardArray[occupiedTileY][occupiedTileX].setFill(Color.WHITE);
         occupiedTileY = y;
         occupiedTileX = x;
-        rectArray[y][x].setFill(Color.ORANGE);
+        boardArray[y][x].setFill(Color.ORANGE);
+    }
+
+    public void checkForFilledRows() {
+        for (int r = 0; r < boardArray.length; r++) {
+            int filledTileCount = 0;
+            for (int c = 0; c < boardArray[r].length; c++) {
+                if (boardArray[r][c].getFill() != Color.WHITE) {
+                     filledTileCount++;
+                }
+            }
+            if (filledTileCount >= boardWidth) {
+                clearLine(r);
+            }
+        }
+    }
+
+    public void clearLine(int rowIndex) {
+        for (int c = 0; c < boardArray[rowIndex].length; c++) {
+            boardArray[rowIndex][c].setFill(Color.WHITE);
+            if (columnHeightArray[c] < boardArray.length) {
+                columnHeightArray[c] += 1;
+            }
+        }
+
+        for (int r = rowIndex--; r >= 0; r--) {
+            for (int c = 0; c < boardArray[r].length; c++) {
+                Paint tempColor = boardArray[r][c].getFill();
+                if (tempColor != Color.WHITE) {
+                    boardArray[r][c].setFill(Color.WHITE);
+                    boardArray[r + 1][c].setFill(tempColor);
+                }
+            }
+        }
     }
 }
