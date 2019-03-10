b6989a9d22e9c5a301dec1d1981c27a4c703386e
Luke-Tran <luketran08@gmail.com>
2019-03-09 22:45:49 -0500

    Refactored by extracting setNewBlockLocation()

--- src/main/java/LukeTetrisPrototype/TetrisPrototype.java
+++ src/main/java/LukeTetrisPrototype/TetrisPrototype.java
@@ -13,12 +13,11 @@ import javafx.scene.input.KeyEvent;
 
 public class TetrisPrototype extends Application {
     int[][] movingTiles = new int[4][2];
+    TetrisBoard board = new TetrisBoard();
 
     public static void main(String[] args) {
         launch();
     }
-
-    TetrisBoard board = new TetrisBoard();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -31,41 +30,17 @@ public class TetrisPrototype extends Application {
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.LEFT) {
                 board.setOccupied(board.occupiedTileY, board.occupiedTileX-1);
-                // TODO - Replace these blocks of code with a method setNewTitleLocation()
-                for (int r = 0; r < board.occupiedTiles.length; r++) {
-                    for (int c = 0; c < board.occupiedTiles[r].length; c++) {
-                        movingTiles[r][c] = board.occupiedTiles[r][c];
-                        if (1 == c) {
-                            movingTiles[r][c] -= 1;
-                        }
-                    }
-                }
+                setNewBlockLocation("left");
                 board.moveTetrimino(movingTiles);
             }
             if (e.getCode() == KeyCode.RIGHT) {
                 board.setOccupied(board.occupiedTileY, board.occupiedTileX+1);
-                // TODO - Replace these blocks of code with a method setNewTitleLocation()
-                for (int pair = 0; pair < board.occupiedTiles.length; pair++) {
-                    for (int xy = 0; xy < board.occupiedTiles[pair].length; xy++) {
-                        movingTiles[pair][xy] = board.occupiedTiles[pair][xy];
-                        if (1 == xy) {
-                            movingTiles[pair][xy] += 1;
-                        }
-                    }
-                }
+                setNewBlockLocation("right");
                 board.moveTetrimino(movingTiles);
             }
             if (e.getCode() == KeyCode.DOWN) {
                 board.setOccupied(board.occupiedTileY+1, board.occupiedTileX);
-                // TODO - Replace these blocks of code with a method setNewTitleLocation()
-                for (int pair = 0; pair < board.occupiedTiles.length; pair++) {
-                    for (int xy = 0; xy < board.occupiedTiles[pair].length; xy++) {
-                        movingTiles[pair][xy] = board.occupiedTiles[pair][xy];
-                        if (0 == xy) {
-                            movingTiles[pair][xy] += 1;
-                        }
-                    }
-                }
+                setNewBlockLocation("down");
                 board.moveTetrimino(movingTiles);
             }
         });
@@ -73,5 +48,31 @@ public class TetrisPrototype extends Application {
         primaryStage.setTitle("Tetris Prototype");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void setNewBlockLocation(String direction) {
+        int dir = 0;
+        int xory = 0;
+        if (direction.equals("left")) {
+            xory = 1;
+            dir = -1;
+        }
+        else if (direction.equals("right")) {
+            xory = 1;
+            dir = 1;
+        }
+        else if (direction.equals("down")) {
+            xory = 0;
+            dir = 1;
+        }
+
+        for (int pair = 0; pair < board.occupiedTiles.length; pair++) {
+            for (int xy = 0; xy < board.occupiedTiles[pair].length; xy++) {
+                movingTiles[pair][xy] = board.occupiedTiles[pair][xy];
+                if (xy == xory) {
+                    movingTiles[pair][xy] += dir;
+                }
+            }
+        }
     }
 }
