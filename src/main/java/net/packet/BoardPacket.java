a1fc90a0424e9fc4ca6f86bc43133fed18494544
Ariel Bobadilla <arielbdilla@gmail.com>
2019-03-15 15:34:50 -0400

    Add creating board packet to/from bytes

--- src/main/java/net/packet/BoardPacket.java
+++ src/main/java/net/packet/BoardPacket.java
@@ -4,9 +4,18 @@ import java.net.DatagramPacket;
 
 public class BoardPacket extends Packet {
 	private int[][] board;
+	private final int BOARD_WIDTH = 10;
+	private final int BOARD_HEIGHT = 24;
 
 	public BoardPacket(DatagramPacket packet) {
+		board = new int[BOARD_HEIGHT][BOARD_WIDTH];
 		String[] data = getDataEntries(packet);
+		String boardData = data[1];
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
+			for (int col = 0; col < BOARD_WIDTH; col++) {
+				board[row][col] = boardData.charAt(row * BOARD_WIDTH + col);
+			}
+		}
 	}
 
 	public BoardPacket(int[][] board) {
@@ -15,6 +24,15 @@ public class BoardPacket extends Packet {
 
 	@Override
 	public byte[] getBytes() {
-		return createByteArray(PacketType.BOARD, null);
+		String boardData = "";
+		StringBuilder sb = new StringBuilder(boardData);
+		for (int[] ints : board) {
+			for (int j = 0; j < board[0].length; j++) {
+				sb.append(ints[j]);
+			}
+		}
+
+		String[] entries = {sb.toString()};
+		return createByteArray(PacketType.BOARD, entries);
 	}
 }
